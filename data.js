c98857bbcb6ab5f1fad10c5e286824ec879d474a
Naruto-070 <naruto2343215@gmail.com>
2025-04-20 15:29:51 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,21 +1,110 @@
 const feelingWheelData = [
   {
-    core: "Happy",
-    color: "#FFD700",
+    core: "Angry",
+    color: "#f94144",
     children: [
-      { primary: "Optimistic", children: ["Hopeful", "Inspired"] },
-      { primary: "Proud", children: ["Confident", "Successful"] },
-      { primary: "Joyful", children: ["Playful", "Content", "Cheerful"] }
+      {
+        primary: "Hurt",
+        children: ["Embarrassed", "Devastated"]
+      },
+      {
+        primary: "Let down",
+        children: ["Betrayed", "Resentful"]
+      },
+      {
+        primary: "Humiliated",
+        children: ["Disrespected", "Ridiculed"]
+      }
+    ]
+  },
+  {
+    core: "Fearful",
+    color: "#f3722c",
+    children: [
+      {
+        primary: "Scared",
+        children: ["Helpless", "Frightened"]
+      },
+      {
+        primary: "Anxious",
+        children: ["Overwhelmed", "Worried"]
+      },
+      {
+        primary: "Insecure",
+        children: ["Inferior", "Worthless"]
+      }
     ]
   },
   {
     core: "Sad",
-    color: "#87CEFA",
+    color: "#577590",
     children: [
-      { primary: "Lonely", children: ["Isolated", "Abandoned"] },
-      { primary: "Depressed", children: ["Empty", "Inferior"] },
-      { primary: "Guilty", children: ["Remorseful", "Ashamed"] }
+      {
+        primary: "Lonely",
+        children: ["Isolated", "Abandoned"]
+      },
+      {
+        primary: "Depressed",
+        children: ["Empty", "Guilty"]
+      },
+      {
+        primary: "Tired",
+        children: ["Sleepy", "Unmotivated"]
+      }
     ]
   },
   {
-    core:
+    core: "Happy",
+    color: "#f9c74f",
+    children: [
+      {
+        primary: "Optimistic",
+        children: ["Hopeful", "Eager"]
+      },
+      {
+        primary: "Proud",
+        children: ["Confident", "Successful"]
+      },
+      {
+        primary: "Joyful",
+        children: ["Cheerful", "Content"]
+      }
+    ]
+  },
+  {
+    core: "Surprised",
+    color: "#90be6d",
+    children: [
+      {
+        primary: "Excited",
+        children: ["Eager", "Energetic"]
+      },
+      {
+        primary: "Amazed",
+        children: ["Astonished", "Awed"]
+      },
+      {
+        primary: "Confused",
+        children: ["Perplexed", "Disoriented"]
+      }
+    ]
+  },
+  {
+    core: "Disgusted",
+    color: "#43aa8b",
+    children: [
+      {
+        primary: "Disapproving",
+        children: ["Judgmental", "Critical"]
+      },
+      {
+        primary: "Awful",
+        children: ["Nauseated", "Detestable"]
+      },
+      {
+        primary: "Repelled",
+        children: ["Revolted", "Horrified"]
+      }
+    ]
+  }
+];
